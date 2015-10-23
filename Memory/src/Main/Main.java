9e83a0899482752a3aa2eb3655c5ae6cf71157e9
Miguel Ángel Campos <miammiel@hotmail.com>
2015-10-23 16:00:22 -0500

    Tasks Asignation

--- Memory/src/Main/Main.java
+++ Memory/src/Main/Main.java
@@ -11,9 +11,31 @@ public class Main {
      * Proyecto Memory 
      * Integrantes:
      *      -Miguel Angel Campos
-     *      -Efrain Perez
+     *              -Clase Ficha
+     *              -Base de Datos
+     *              -Parte Grafica de Clase Board
+     *      -Efrain Perez                   
+     *              -Peticion de Edad, Sexo previo al inicio del juego
+     *              -Metodo Validador de clicks
+     *              -Logica Juego Segunda Ronda
      *      -Jonathan Amaya
+     *              -Presentacion 
+     *              -Timer
+     *              -Miscelaneo
      *      -Ulises Madero 
+     *              -Metodo de generacion de pares de numeros 
+     *              -Logica Juego primera Ronda
+     *              -Miscelaneo
+     */
+    
+    
+    /**
+     * Normas:
+     *     -Mantener Orden
+     *     -Mantener Identacion de codigo (Format)
+     *     -Usar Estrictamente Paradigma Orientado a Objetos
+     *     -Base de datos emulada en XAMP PhpMyAdmin MySQL
+     *     -**En cada metodo asignado Agregar en comentarios Fecha hora y tiempo de trabajo
      */
     public static void main(String[] args) {
         Context g = new Context();
